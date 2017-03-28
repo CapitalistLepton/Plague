a48df25d9183e0a6d8d546f4ad58fe915dfc1ad9
Zane Littrell <littrellzane@gmail.com>
2017-03-27 20:09:51 -0700

    Moved generatePanels to constructor method. Added hotkeys for the first 4 antibiotics.

--- src/main/java/com/capitalistlepton/plague/view/MainWindow.java
+++ src/main/java/com/capitalistlepton/plague/view/MainWindow.java
@@ -6,6 +6,7 @@ import com.capitalistlepton.plague.model.Antibiotic;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 /**
  * Created by zanelittrell on 2/3/17.
@@ -31,12 +32,6 @@ public class MainWindow extends JFrame {
             this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         }
 
-        generatePanels(instance);
-
-        this.setVisible(true);
-    }
-
-    private JPanel generatePanels(Plague instance) {
         Container pane = this.getContentPane();
         pane.setBackground(BACKGROUND);
         pane.setLayout(new GridBagLayout());
@@ -60,9 +55,11 @@ public class MainWindow extends JFrame {
         pane.add(stats, c);
 
         controlBar = new JPanel();
+        ArrayList<AntibioticCheckBox> boxes = new ArrayList<AntibioticCheckBox>(Antibiotic.values().length);
         for (Antibiotic a : Antibiotic.values()) {
             AntibioticCheckBox check = new AntibioticCheckBox(a, instance);
             controlBar.add(check);
+            boxes.add(check);
         }
         controlBar.setBackground(BACKGROUND);
         c.weightx = 0.8;
@@ -77,20 +74,52 @@ public class MainWindow extends JFrame {
         c.gridy = 1;
         pane.add(money, c);
 
-        money.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("F2"),
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("F2"),
                 "restart");
-        money.getActionMap().put("restart",
+        controlBar.getActionMap().put("restart",
                 new Plague.RestartPlague());
-        money.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("Q"),
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("Q"),
                 "quit");
-        money.getActionMap().put("quit",
+        controlBar.getActionMap().put("quit",
                 new AbstractAction() {
                     public void actionPerformed(ActionEvent e) {
                         System.exit(0);
                     }
                 });
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("1"),
+                "select1");
+        controlBar.getActionMap().put("select1",
+                new AbstractAction() {
+                    public void actionPerformed(ActionEvent e) {
+                        boxes.get(0).setSelected(!boxes.get(0).isSelected());
+                    }
+                });
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("2"),
+                "select2");
+        controlBar.getActionMap().put("select2",
+                new AbstractAction() {
+                    public void actionPerformed(ActionEvent e) {
+                        boxes.get(1).setSelected(!boxes.get(1).isSelected());
+                    }
+                });
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("3"),
+                "select3");
+        controlBar.getActionMap().put("select3",
+                new AbstractAction() {
+                    public void actionPerformed(ActionEvent e) {
+                        boxes.get(2).setSelected(!boxes.get(2).isSelected());
+                    }
+                });
+        controlBar.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("4"),
+                "select4");
+        controlBar.getActionMap().put("select4",
+                new AbstractAction() {
+                    public void actionPerformed(ActionEvent e) {
+                        boxes.get(3).setSelected(!boxes.get(3).isSelected());
+                    }
+                });
 
-        return money;
+        this.setVisible(true);
     }
 
     public void displayMessage(String message) { bacteria.writeMessage(message); }
